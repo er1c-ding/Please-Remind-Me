9209c8b93318726ec15672de51b81f3e93caf9d5
Eric Ding <dingguanbo1215@gmail.com>
2026-09-02 22:50:09 -0400

    Update reminders.py
    Create class architecture

--- reminders.py
+++ reminders.py
@@ -6,6 +6,33 @@ class Reminder():
     def __init__(self, enabled):
         self.reminder_id = Reminder.reminder_count
         Reminder.reminder_count += 1
-
-        self.start_time = datetime.now().replace(microsecond=0)
         self.enabled = enabled
+
+class Recurring_Reminder(Reminder):
+    def __init__(self, enabled, interval):
+        super().__init__(enabled)
+        self.interval = interval
+        self.start_time = datetime.now().replace(microsecond=0)
+        self.next_time = None
+
+class One_Time_Reminder(Reminder):
+    def __init__(self, enabled, date, time):
+        super().__init__(enabled)
+        self.time = None
+
+class Daily_Reminder(Reminder):
+    def __init__(self, enabled, days, time):
+        super().__init__(enabled)
+        self.time = None
+
+class Podomuro_Reminder(Reminder):
+    def __init__(self, enabled, cycle):
+        super().__init__(enabled)
+        self.start_time = datetime.now().replace(microsecond=0)
+        self.next_time = None
+
+class Twenty_Reminder(Reminder):
+    def __init__(self, enabled, cycle):
+        super().__init__(enabled)
+        self.start_time = datetime.now().replace(microsecond=0)
+        self.next_time = None
